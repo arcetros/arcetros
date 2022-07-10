a142474d3f54e77702e184548571deacd8aea1bc
arcetros <75669794+arcetros@users.noreply.github.com>
2022-07-10 21:58:20 +0800

    Minor fix to index page

---
+++ components/Footer.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BsGithub, BsFillEnvelopeFill } from 'react-icons/bs';
+
+interface ISocials {
+  icon: React.ReactNode;
+  text: string;
+}
+
+const Socials = ({ icon, text }: ISocials) => (
+  <div className="flex items-center gap-x-2 text-secondary hover:text-black cursor-pointer">
+    {icon}
+    <span>{text}</span>
+  </div>
+);
+
+const Footer = () => (
+  <footer className="mt-[9.875rem]">
+    <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-y-4">
+      <div>
+        <p>
+          Created by{' '}
+          <span className="hover:underline cursor-pointer">arcetros</span>
+        </p>
+        <p>Built with Next JS</p>
+      </div>
+      <div className="flex">
+        {/* Change this to next link later */}
+        <div className="flex items-center gap-x-4">
+          <Socials icon={<BsGithub />} text="arcetros" />
+          <Socials icon={<BsFillEnvelopeFill />} text="0arcetros@gmail.com" />
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,47 @@
 import type { NextPage } from 'next';
 import Layout from '../components/Layout';
+import Footer from '../components/Footer';
+import ProjectCard from '../components/ProjectCard';
 
 const Home: NextPage = () => (
   <Layout>
     <p className="text-primary text-2xl">hello, world!</p>
     <h1 className="text-secondary mt-4 w-full">
       <p>
-        my name is Mufid Arkanu and i&#39;m a front end developer based on
-        Indonesia
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis,
+        obcaecati!
       </p>
     </h1>
     <p className="text-secondary mt-4">
-      i mainly work within the React ecosystem, i&#39;ve been sharpen my skills
-      and building projects to apply what i&#39;ve learnt.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit praesentium
+      illum voluptate architecto voluptas eum minima, nemo exercitationem
+      asperiores reprehenderit.
+    </p>
+    <p className="text-secondary mt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. At minima
+      blanditiis itaque earum, maxime quidem voluptates architecto molestiae
+      expedita assumenda accusamus id quod sapiente autem ratione officiis
+    </p>
+    <p className="text-secondary mt-4">
+      quibusdam dolores consequuntur iure eum et! Itaque, officia porro sequi in
+      facilis officiis atque neque perferendis est reprehenderit rerum excepturi
+      animi fuga cupiditate!
     </p>
 
     <h2 className="text-primary text-2xl mt-[7.3125rem]">
       things i&#39;ve built:{' '}
     </h2>
     <div className="min-h-fit mt-4">
-      <div className="grid grid-cols-1 lg:grid-cols-3 place-content-evenly  gap-x-4 gap-y-[4.75rem]">
-        <div className="border border-secondary min-h-[228px] max-w-[18.3125rem] shadow-box max-h-max">
-          t
-        </div>
-        <div className="border border-secondary min-h-[228px] max-w-[18.3125rem] shadow-box max-h-max">
-          t
-        </div>
-        <div className="border border-secondary min-h-[228px] max-w-[18.3125rem] shadow-box max-h-max">
-          t
-        </div>
-        <div className="border border-secondary min-h-[228px] max-w-[18.3125rem] shadow-box max-h-max">
-          t
-        </div>
-        <div className="border border-secondary min-h-[228px] max-w-[18.3125rem] shadow-box max-h-max">
-          t
-        </div>
-        <div className="border border-secondary min-h-[228px] max-w-[18.3125rem] shadow-box max-h-max">
-          t
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-[4.75rem]">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
       </div>
     </div>
-    <footer className="mt-[9.875rem]">
-      <div className="flex justify-between items-center">
-        <div>
-          <p>Created by arcetros</p>
-          <p>Built with Next JS</p>
-        </div>
-        <div>socials</div>
-      </div>
-    </footer>
+    <Footer />
   </Layout>
 );
 
