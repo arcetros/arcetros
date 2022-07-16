885032ef9de5d871566d3c35d559f9ec15719fb8
arcetros <75669794+arcetros@users.noreply.github.com>
2022-07-16 21:51:42 +0800

    refactor(hero): use empty div instead of css border (for animation later)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import Image from 'next/image';
-import { BsTwitter, BsGithub, BsEnvelopeFill } from 'react-icons/bs';
+import { FiTwitter, FiGithub, FiInbox } from 'react-icons/fi';
 import useMobile from '../hooks/useMobile';
 
 const Hero = () => {
   const { isMobile } = useMobile();
   return (
     <div className="grid grid-cols-1 lg:grid-cols-12 grid-rows-9 h-auto lg:h-screen">
-      <div className="relative flex row-span-4 col-span-1 lg:col-span-7 border-b border-black">
-        <div className="my-auto w-full px-4 py-8 lg:px-[7.1875rem]">
+      <div className="relative flex row-span-4 col-span-1 lg:col-span-7">
+        <div
+          style={{ width: '100%' }}
+          className="absolute h-[1px] bottom-0 bg-black"
+        />
+        <div className="my-auto w-full px-4 lg:px-[7.1875rem]">
           <h1 className="text-[1.5rem] font-primary">arcetros</h1>
-          <h1 className="text-gray-400 mt-2 text-base font-secondary">
-            <span>Front End/UI UX Developer</span>
+          <h1 className="text-gray-600 mt-2 text-base font-headline">
+            <span>front-end/ui ux developer</span>
           </h1>
-          <div className="flex gap-x-10 mt-12 lg:mt-[6.5625rem] font-secondary">
+          <div className="flex gap-x-10 mt-12 lg:mt-[6.5625rem] font-headline">
             <p>ABOUT</p>
             <p>PROJECT</p>
             <p>CONTACT</p>
           </div>
         </div>
       </div>
-      <div className="row-span-6 col-span-1 lg:col-span-4 border-b border-r border-l border-black">
-        <div className="relative min-h-[40vh] lg:min-h-0 h-full w-full">
+      <div className="relative row-span-6 col-span-1 lg:col-span-4 ">
+        <div
+          style={{ width: '100%' }}
+          className="absolute h-[0.5px] -bottom-[0.5px] right-0 bg-black z-10"
+        />
+        <div
+          style={{ height: '100%' }}
+          className="absolute w-[0.5px] bottom-0 bg-black z-10 hidden lg:block"
+        />
+        <div
+          style={{ height: '100%' }}
+          className="absolute w-[0.5px] top-0 right-0 bg-black z-10"
+        />
+        <div className="relative min-h-[40vh] lg:min-h-0 h-full w-full z-0">
           <Image
             src="/download.jpg"
             alt="Photo"
@@ -31,28 +47,32 @@ const Hero = () => {
           />
         </div>
       </div>
-      <div className="order-last lg:order-none row-span-4 col-span-1 lg:col-span-1 border-b border-black my-4 lg:my-0">
+      <div className="relative order-last lg:order-none row-span-4 col-span-1 lg:col-span-1 my-4 lg:my-0">
+        <div
+          style={{ width: '100%' }}
+          className="absolute h-[0.5px] -bottom-[0.5px] right-0 bg-black z-10"
+        />
         <div className="flex mx-auto h-full gap-x-5 items-center px-4 py-8 lg:py-0 lg:px-0">
           <div className="block lg:hidden">My Socials: </div>
-          <div className="flex gap-x-5 mx-0 lg:mx-auto flex-row lg:flex-col h-full justify-evenly">
+          <div className="flex gap-x-5 mx-0 lg:mx-auto flex-row lg:flex-col h-full justify-evenly font-headline">
             {isMobile && <span className="underline">Twitter</span>}
             {!isMobile && (
-              <BsTwitter className="mx-auto w-8 h-8 text-[#484848]" />
+              <FiTwitter className="mx-auto w-7 h-7 text-[#484848]" />
             )}
             {isMobile && <span className="underline">Github</span>}
             {!isMobile && (
-              <BsGithub className="mx-auto w-8 h-8 text-[#484848]" />
+              <FiGithub className="mx-auto w-7 h-7 text-[#484848]" />
             )}
             {isMobile && <span className="underline">Email</span>}
             {!isMobile && (
-              <BsEnvelopeFill className="mx-auto w-8 h-8 text-[#484848]" />
+              <FiInbox className="mx-auto w-7 h-7 text-[#484848]" />
             )}
           </div>
         </div>
       </div>
       <div className="flex row-span-5 col-span-1 lg:col-span-7 border-b border-black lg:border-none">
         <div className="px-4 my-8 lg:px-[7.1875rem] w-full lg:my-auto">
-          <p className="text-3xl font-secondary">
+          <p className="text-primary text-3xl font-secondary">
             Solving real user problems and create thoughtful and beautiful
             experiences are root of this work
           </p>
@@ -60,7 +80,7 @@ const Hero = () => {
             <div className="w-fit relative group scale-[1.01] hover:scale-100 transition duration-300 ease-in-out h-full">
               <button
                 type="button"
-                className="bg-slate-600 text-gray-50 font-medium font-secondary rounded-md w-full border-slate-700 solid border p-3"
+                className="bg-slate-600 text-gray-50 font-medium font-headline rounded-md w-full border-slate-700 solid border p-3"
               >
                 GET IN TOUCH
               </button>
@@ -69,7 +89,7 @@ const Hero = () => {
             <div className="w-fit relative group scale-[1.01] hover:scale-100 transition duration-300 ease-in-out h-full">
               <button
                 type="button"
-                className="bg-white text-slate-800 font-medium font-secondary rounded-md w-full border-slate-800 solid border p-3"
+                className="bg-white text-slate-800 font-medium font-headline rounded-md w-full border-slate-800 solid border p-3"
               >
                 SEE PROJECTS
               </button>
@@ -78,13 +98,18 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="flex border-l-0 lg:border-l border-b lg:border-b-0 border-black row-span-3 col-span-1 order-2 lg:order-last lg:col-span-4">
+      <div className="relative flex row-span-3 col-span-1 order-2 lg:order-last lg:col-span-4">
+        <div
+          style={{ height: '100%' }}
+          className="absolute w-[0.5px] bottom-0 left-0 bg-black z-10 hidden lg:block"
+        />
         <div className="container font-primary px-4 my-8 lg:m-auto">
-          <p>Hello! my name is Mufid Arkanu</p>
-          <p className="mt-2">
-            My love of beautiful websites made me curious about how they were
-            made. I adore building accessible, responsive and fast code. Read
-            more
+          <p>Hi! I&#39;m Mufid.</p>
+          <p className="mt-4">
+            I&#39;am a{' '}
+            <strong className="font-headline">front-end developer</strong> with
+            a passion of building accessible, responsive and fast performant
+            JAMstack sites !
           </p>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -7,7 +9,7 @@ module.exports = {
     extend: {
       colors: {
         background: '#F5E4BC',
-        primary: '#CA6B63',
+        primary: '#302f2f',
         secondary: '#4D4A36'
       },
       gridTemplateRows: { 9: 'repeat(9, minmax(0, 1fr))' },
@@ -15,6 +17,7 @@ module.exports = {
         box: '10px 10px 0px 0px rgba(0, 0, 0, 1)'
       },
       fontFamily: {
+        headline: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono],
         primary: ['Grotesk Regular', 'Grotesk Medium', 'Grotesk Bold'],
         secondary: ['Lausanne']
       }
